acef9ccc3d307b18cf877db8e0e420fb01901bbc
Peter Spada <peterspa@microsoft.com>
2025-07-25 10:14:41 -0700

    Initial commit

---
+++ LiveCalcCore/CalcModel.cs
@@ -0,0 +1,115 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace LiveCalcCore
+{
+    public partial class CalcModel : ObservableObject
+    {
+        [ObservableProperty]
+        private decimal result;
+
+        [RelayCommand]
+        private void Execute(Command command)
+        {
+            switch (command)
+            {
+                case Command.None:
+                    break;
+
+                case Command.Type0:
+                    break;
+
+                case Command.Type1:
+                    break;
+
+                case Command.Type2:
+                    break;
+
+                case Command.Type3:
+                    break;
+
+                case Command.Type4:
+                    break;
+
+                case Command.Type5:
+                    break;
+
+                case Command.Type6:
+                    break;
+
+                case Command.Type7:
+                    break;
+
+                case Command.Type8:
+                    break;
+
+                case Command.Type9:
+                    break;
+
+                case Command.DecimalPoint:
+                    break;
+
+                case Command.Backspace:
+                    break;
+
+                case Command.ClearAll:
+                    break;
+
+                case Command.ClearEntry:
+                    break;
+
+                case Command.Plus:
+                    break;
+
+                case Command.Minus:
+                    break;
+
+                case Command.Multiply:
+                    break;
+
+                case Command.Divide:
+                    break;
+
+                case Command.Equal:
+                    break;
+
+                case Command.PlusMinus:
+                    break;
+
+                case Command.Percent:
+                    break;
+
+                case Command.SquareRoot:
+                    break;
+
+                case Command.Square:
+                    break;
+
+                case Command.Inverse:
+                    break;
+
+                case Command.ConstantPi:
+                    break;
+
+                case Command.MemoryClear:
+                    break;
+
+                case Command.MemoryRecall:
+                    break;
+
+                case Command.MemoryAdd:
+                    break;
+
+                case Command.MemorySubtract:
+                    break;
+
+                case Command.MemoryStore:
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+    }
+}
---
+++ LiveCalcCore/Command.cs
@@ -0,0 +1,47 @@
+﻿namespace LiveCalcCore
+{
+    public enum Command
+    {
+        None,
+
+        // Typing
+        Type0,
+        Type1,
+        Type2,
+        Type3,
+        Type4,
+        Type5,
+        Type6,
+        Type7,
+        Type8,
+        Type9,
+        DecimalPoint,
+        Backspace,
+        ClearAll,
+        ClearEntry,
+
+        // Binary operations
+        Plus,
+        Minus,
+        Multiply,
+        Divide,
+
+        // Unary operations
+        Equal,
+        PlusMinus,
+        Percent,
+        SquareRoot,
+        Square,
+        Inverse,
+
+        // Constants
+        ConstantPi,
+
+        // Memory operations
+        MemoryClear,
+        MemoryRecall,
+        MemoryAdd,
+        MemorySubtract,
+        MemoryStore,
+    }
+}
