fa3dff8c998999ce688a9030c15aa607779dc309
Klaus Löffelmann <klaus.loeffelmann@microsoft.com>
2025-08-06 14:51:21 -0700

    Introduce central package (version) management.
    * Make WinForms App ServiceProvider compatible.
    * Introduce ASP.NET compatible DI boot strapping.

--- LiveCalcWinForms/MainForm.Designer.cs
+++ LiveCalcWinForms/MainForm.Designer.cs
@@ -1,39 +1,102 @@
-﻿namespace CalcWinForms
+﻿using Microsoft.Extensions.DependencyInjection;
+using WarpToolkit.ComponentModel;
+
+namespace CalcWinForms;
+
+partial class MainForm : IServiceProvider
 {
-    partial class MainForm
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    private readonly IUserSettingsService _userSettingsService = null;
+    private readonly IServiceProvider _serviceProvider = null;
+
+    /// <summary>
+    ///  Initializes a new instance of the <see cref="MainForm"/> class with dependency injection support.
+    /// </summary>
+    /// <param name="serviceProvider">
+    ///  The service provider that contains all registered services for dependency injection.
+    ///  This parameter is used to resolve dependencies and configure the form with the required services.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    ///  Thrown when <paramref name="serviceProvider"/> is <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="NullReferenceException">
+    ///  Thrown when the required <see cref="IUserSettingsService"/> is not registered in the service provider.
+    /// </exception>
+    /// <remarks>
+    ///  This constructor overload is specifically designed to be used when the Form is instantiated 
+    ///  through Dependency Injection (DI) using the <c>WinFormsApplication</c> class and the 
+    ///  <c>WinFormsApplicationBuilder</c>. This approach provides the same infrastructure pattern 
+    ///  as ASP.NET Core applications, enabling familiar service registration, configuration, 
+    ///  and dependency injection patterns in WinForms applications.
+    ///  <para>
+    ///   When using this constructor, the Form acts as a ServiceProvider-aware component, 
+    ///   allowing it to resolve and utilize services that have been registered in the 
+    ///   application's service container. This enables loose coupling, testability, 
+    ///   and modern application architecture patterns in WinForms development.
+    ///  </para>
+    ///  <para>
+    ///   The constructor automatically assigns the service provider to the form using the 
+    ///   <c>AssignServiceProvider</c> extension method and resolves the required 
+    ///   <see cref="IUserSettingsService"/> from the container.
+    ///  </para>
+    /// </remarks>
+    public MainForm(IServiceProvider serviceProvider)
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        ArgumentNullException.ThrowIfNull(serviceProvider, nameof(serviceProvider));
+        _serviceProvider = serviceProvider;
+        _userSettingsService = serviceProvider.GetRequiredService<IUserSettingsService>();
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        if (_userSettingsService is null)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            throw new NullReferenceException($"The service '{nameof(IUserSettingsService)}' is not registered.");
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            components = new System.ComponentModel.Container();
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
-        }
-
-        #endregion
+        InitializeComponent();
     }
+
+    object IServiceProvider.GetService(Type serviceType)
+    {
+        ArgumentNullException.ThrowIfNull(serviceType, nameof(serviceType));
+
+        if (_serviceProvider is null)
+        {
+            throw new InvalidOperationException("Service provider is not initialized.");
+        }
+
+        return _serviceProvider.GetService(serviceType)
+            ?? throw new InvalidOperationException($"Service of type '{serviceType.Name}' is not registered.");
+    }
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        components = new System.ComponentModel.Container();
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Text = "Form1";
+    }
+
+    #endregion
 }
--- LiveCalcWinForms/Program.cs
+++ LiveCalcWinForms/Program.cs
@@ -1,17 +1,46 @@
-namespace CalcWinForms
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.WinForms;
+using System.Diagnostics;
+using WarpToolkit.WinForms.AppServices.ServiceExtensions;
+
+namespace CalcWinForms;
+
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
-        }
+        WinFormsApplicationBuilder builder = WinFormsApplication.CreateBuilder();
+
+        // We want to use the UserSettings service, for a convenient
+        // way to store user settings in a file.
+        builder.Services.AddWinFormsUserSettingsService();
+
+        // We want to use the Exception service, so we can handle
+        // unhandled exceptions in a consistent way.
+        builder.Services.AddWinFormsExceptionService();
+
+        Debug.Assert(Thread.CurrentThread.GetApartmentState() == ApartmentState.STA);
+
+        builder.Services.AddScoped<MainForm>();
+
+        // Configure WinForms-specific options
+        builder.UseStartupForm<MainForm>()
+            // Setting HighDpiMode to SystemAware:
+            .UseHighDpiMode(HighDpiMode.SystemAware)
+            // Setting the DarkMode to System.
+            .UseColorMode(SystemColorMode.System)
+            // Equals to SetCompatibleTextRenderingDefault(false)
+            .UseTextRenderingV2()
+            // This is default for Windows 10+ and later.
+            .UseVisualStyles();
+
+        // Build and run the application
+        WinFormsApplication app = builder.Build();
+
+        app.Run();
     }
 }
